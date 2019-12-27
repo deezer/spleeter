fc1e3d9a2f620c481f9d876ee142be8787abc3d9
romi1502 <rhennequin@deezer.com>
2019-12-27 14:34:27 +0100

    Added new config for processing until 16kHz

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ __license__ = 'MIT License'
 
 # Default project values.
 project_name = 'spleeter'
-project_version = '1.4.8'
+project_version = '1.4.9'
 tensorflow_dependency = 'tensorflow'
 tensorflow_version = '1.14.0'
 here = path.abspath(path.dirname(__file__))

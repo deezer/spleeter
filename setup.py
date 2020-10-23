1181f4b54d5ca3b9c9255a17d3d5084a510d7737
Félix Voituret <Faylixe@users.noreply.github.com>
2020-10-23 14:14:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ __license__ = 'MIT License'
 
 # Default project values.
 project_name = 'spleeter'
-project_version = '2.0'
+project_version = '2.0.1'
 tensorflow_dependency = 'tensorflow'
 tensorflow_version = '2.3.0'
 here = path.abspath(path.dirname(__file__))

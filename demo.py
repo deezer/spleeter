65db3b12d5c32fa971413c7dddf7f317d2b9e909
AK391 <ahsengradio@gmail.com>
2021-04-09 15:16:32 -0400

    desc update and sample

--- demo.py
+++ demo.py
@@ -17,11 +17,11 @@ inputs = [
 ]
 outputs =  gr.outputs.Audio(label="Output Audio", type="file")
 
-title = "wav2vec 2.0"
-description = "demo for Facebook AI wav2vec 2.0 using Hugging Face transformers. To use it, simply upload your audio, or click one of the examples to load them. Read more at the links below."
-article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2006.11477'>wav2vec 2.0: A Framework for Self-Supervised Learning of Speech Representations</a> | <a href='https://github.com/pytorch/fairseq'>Github Repo</a> | <a href='https://huggingface.co/facebook/wav2vec2-base-960h'>Hugging Face model</a></p>"
+title = "Spleeter"
+description = "demo for Spleeter by Deezer. To use it, simply upload your audio, or click one of the examples to load them. Read more at the links below."
+article = "<p style='text-align: center'><a href='https://research.deezer.com/projects/spleeter.html'>Spleeter: a Fast and Efficient Music Source Separation Tool with Pre-Trained Models</a> | <a href='https://github.com/deezer/spleeter'>Github Repo</a></p>"
 examples = [
     ["audio_example.mp3"]
 ]
 
-gr.Interface(spleeter, inputs, outputs, title=title, description=description, article=article, examples=examples).launch(debug=True)
+gr.Interface(spleeter, inputs, outputs, title=title, description=description, article=article, examples=examples).launch()
